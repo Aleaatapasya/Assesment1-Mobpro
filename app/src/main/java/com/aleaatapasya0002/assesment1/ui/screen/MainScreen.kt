69f62a853940fe42aa4bef3cf981db3c528d62fc
Alea Atapasya <aleaatpsya@gmail.com>
2025-03-26 19:24:24 +0700

    Membuat tombol input

--- app/src/main/java/com/aleaatapasya0002/assesment1/ui/screen/MainScreen.kt
+++ app/src/main/java/com/aleaatapasya0002/assesment1/ui/screen/MainScreen.kt
@@ -10,24 +10,33 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.aleaatapasya0002.assesment1.R
@@ -56,6 +65,22 @@ fun MainScreen() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ScreenContent(modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.gambar_intro)
+
+    val satuanBerat =
+        arrayOf(R.string.kg, R.string.hg, R.string.dag, R.string.gram, R.string.dg, R.string.cg, R.string.mg)
+
+    val selectedAwal = remember { mutableIntStateOf(satuanBerat[0]) }
+    val selectedAkhir = remember { mutableIntStateOf(satuanBerat[1]) }
+
+    val expandedAwal = remember { mutableStateOf(false) }
+    val expandedAkhir = remember { mutableStateOf(false) }
+
+    var berat by remember { mutableStateOf("") }
+    val beratError by remember { mutableStateOf(false) }
+
+
+
     Column(
         modifier = modifier
             .fillMaxSize()
@@ -68,19 +93,12 @@ fun ScreenContent(modifier: Modifier = Modifier) {
             modifier = Modifier
                 .fillMaxWidth()
         )
-        val image = painterResource(R.drawable.gambar_intro)
         Image(
             painter = image,
             contentDescription = stringResource(id = R.string.gambar),
             modifier = modifier
                 .size(180.dp)
         )
-        val satuanBerat =
-            arrayOf(R.string.kg, R.string.hg, R.string.dag, R.string.gram, R.string.dg, R.string.cg, R.string.mg)
-        val selectedAwal = remember { mutableIntStateOf(satuanBerat[0]) }
-        val selectedAkhir = remember { mutableIntStateOf(satuanBerat[1]) }
-        val expandedAwal = remember { mutableStateOf(false) }
-        val expandedAkhir = remember { mutableStateOf(false) }
         Row(
             modifier = Modifier
                 .fillMaxWidth(),
@@ -163,6 +181,44 @@ fun ScreenContent(modifier: Modifier = Modifier) {
                 }
             }
         }
+            OutlinedTextField(
+                value = berat,
+                onValueChange = { berat = it},
+                label = {Text(text = stringResource(R.string.input))},
+                trailingIcon = {IconPicker(beratError, getInitial(stringResource( selectedAwal.intValue))) },
+                supportingText = { ErrorHint(beratError) },
+                isError = beratError,
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number,
+                    imeAction = ImeAction.Done
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 10.dp)
+
+            )
+        }
+    }
+
+fun getInitial(unit: String): String{
+    return unit.takeLast(4)
+}
+
+@Composable
+fun IconPicker(isError: Boolean, unit: String) {
+    if (isError){
+        Icon(imageVector = Icons.Filled.Warning, contentDescription = null)
+    }
+    else{
+        Text(text = unit)
+    }
+}
+
+@Composable
+fun ErrorHint(isError: Boolean) {
+    if (isError){
+        Text(text = stringResource(R.string.input_invalid))
     }
 }
 
