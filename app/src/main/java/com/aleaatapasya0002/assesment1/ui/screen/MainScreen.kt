ca4ccaf8775ce5bada923ef416f42bf1da8b679e
Alea Atapasya <aleaatpsya@gmail.com>
2025-03-26 17:54:33 +0700

    Membuat dropdown untuk memilih satuan berat

--- app/src/main/java/com/aleaatapasya0002/assesment1/ui/screen/MainScreen.kt
+++ app/src/main/java/com/aleaatapasya0002/assesment1/ui/screen/MainScreen.kt
@@ -2,18 +2,28 @@ package com.aleaatapasya0002.assesment1.ui.screen
 
 import android.content.res.Configuration
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -43,6 +53,7 @@ fun MainScreen() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ScreenContent(modifier: Modifier = Modifier) {
     Column(
@@ -64,10 +75,96 @@ fun ScreenContent(modifier: Modifier = Modifier) {
             modifier = modifier
                 .size(180.dp)
         )
-
+        val satuanBerat =
+            arrayOf(R.string.kg, R.string.hg, R.string.dag, R.string.gram, R.string.dg, R.string.cg, R.string.mg)
+        val selectedAwal = remember { mutableIntStateOf(satuanBerat[0]) }
+        val selectedAkhir = remember { mutableIntStateOf(satuanBerat[1]) }
+        val expandedAwal = remember { mutableStateOf(false) }
+        val expandedAkhir = remember { mutableStateOf(false) }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(end = 3.dp)
+                    .weight(1f)
+            ) {
+                ExposedDropdownMenuBox(
+                    expanded = expandedAwal.value,
+                    onExpandedChange = {
+                        expandedAwal.value = !expandedAwal.value
+                    }
+                ) {
+                    TextField(
+                        value = stringResource(selectedAwal.intValue),
+                        onValueChange = {},
+                        readOnly = true,
+                        label = { Text(text = stringResource(R.string.satuan_awal)) },
+                        trailingIcon = {
+                            ExposedDropdownMenuDefaults.TrailingIcon(expanded = expandedAwal.value)
+                        },
+                        modifier = Modifier.menuAnchor()
+                    )
+                    ExposedDropdownMenu(
+                        expanded = expandedAwal.value,
+                        onDismissRequest = { expandedAwal.value = false }
+                    ) {
+                        satuanBerat.forEach { resId ->
+                            DropdownMenuItem(
+                                text = { Text(text = stringResource(resId)) },
+                                onClick = {
+                                    selectedAwal.intValue = resId
+                                    expandedAwal.value = false
+                                }
+                            )
+                        }
+                    }
+                }
+            }
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(end = 3.dp)
+                    .weight(1f)
+            ) {
+                ExposedDropdownMenuBox(
+                    expanded = expandedAkhir.value,
+                    onExpandedChange = {
+                        expandedAkhir.value = !expandedAkhir.value
+                    }
+                ) {
+                    TextField(
+                        value = stringResource(selectedAkhir.intValue),
+                        onValueChange = {},
+                        readOnly = true,
+                        label = { Text(text = stringResource(R.string.satuan_akhir)) },
+                        trailingIcon = {
+                            ExposedDropdownMenuDefaults.TrailingIcon(expanded = expandedAkhir.value)
+                        },
+                        modifier = Modifier.menuAnchor()
+                    )
+                    ExposedDropdownMenu(
+                        expanded = expandedAkhir.value,
+                        onDismissRequest = { expandedAkhir.value = false }
+                    ) {
+                        satuanBerat.forEach { resId ->
+                            DropdownMenuItem(
+                                text = { Text(text = stringResource(resId)) },
+                                onClick = {
+                                    selectedAkhir.intValue = resId
+                                    expandedAkhir.value = false
+                                }
+                            )
+                        }
+                    }
+                }
+            }
+        }
     }
 }
-
 
 @Preview(showBackground = true)
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
