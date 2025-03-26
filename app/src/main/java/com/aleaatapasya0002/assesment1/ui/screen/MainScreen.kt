c2b4c418da50d933b001f026e7259085dbeb735f
Alea Atapasya <aleaatpsya@gmail.com>
2025-03-26 15:28:31 +0700

    Menambahkan gambar untuk intro

--- app/src/main/java/com/aleaatapasya0002/assesment1/ui/screen/MainScreen.kt
+++ app/src/main/java/com/aleaatapasya0002/assesment1/ui/screen/MainScreen.kt
@@ -1,7 +1,12 @@
 package com.aleaatapasya0002.assesment1.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -9,9 +14,12 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.aleaatapasya0002.assesment1.R
 import com.aleaatapasya0002.assesment1.ui.theme.Assesment1Theme
 
@@ -37,11 +45,29 @@ fun MainScreen() {
 
 @Composable
 fun ScreenContent(modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello Android!",
+    Column(
         modifier = modifier
-    )
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = stringResource(id = R.string.intro),
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier
+                .fillMaxWidth()
+        )
+        val image = painterResource(R.drawable.gambar_intro)
+        Image(
+            painter = image,
+            contentDescription = stringResource(id = R.string.gambar),
+            modifier = modifier
+                .size(180.dp)
+        )
+
+    }
 }
+
 
 @Preview(showBackground = true)
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
