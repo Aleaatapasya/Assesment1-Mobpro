f255dd1d63a4a453868f7f82e36911ee3a72a112
Alea Atapasya <aleaatpsya@gmail.com>
2025-03-30 08:33:09 +0700

    Membuat tampilan about

--- app/src/main/java/com/aleaatapasya0002/assesment1/navigation/Screen.kt
+++ app/src/main/java/com/aleaatapasya0002/assesment1/navigation/Screen.kt
@@ -2,4 +2,5 @@ package com.aleaatapasya0002.assesment1.navigation
 
 sealed class Screen(val route: String) {
     data object Home:Screen("mainScreen")
+    data object About:Screen("aboutScreen")
 }
